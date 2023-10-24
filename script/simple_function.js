0d7c0752f35a4054d2c7bbe34df3516fece7e762
jtoxquimaxil <jtoxquimaxil@gmail.com>
2023-10-24 11:29:21 -0400

    exercise 1 completed

---
+++ script/simple_function.js
@@ -0,0 +1,21 @@
+"use strict";
+
+function favoriteThings() {
+    console.log("Jakelin");
+    console.log("Beauty and the Beast");
+    console.log("Bruno Mars");
+}
+
+
+function whyImHere(){
+    console.log("I joined Learning to Code program to learn about something new");
+}
+
+
+function favoritePlace(){
+    console.log("My Home");
+}
+
+favoritePlace();
+favoriteThings();
+whyImHere();
