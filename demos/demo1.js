453451d94284e467d0a09b9e8b8fa8087b27a034
jtoxquimaxil <jtoxquimaxil@gmail.com>
2023-10-24 11:16:53 -0400

    Finished demo1

---
+++ demos/demo1.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function doSomething(){
+    //some code goes here. 
+    console.log("doSomething")
+}
+
+function doSomethingElse(){
+    console.log("else");
+}
+
+doSomething();
+doSomething();
+doSomethingElse();
