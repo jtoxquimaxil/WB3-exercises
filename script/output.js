a404491afabd3cc8af6dab38dd264d3871eda09f
jtoxquimaxil <jtoxquimaxil@gmail.com>
2023-10-25 21:51:25 -0400

    finished exercise 1

---
+++ script/output.js
@@ -0,0 +1,30 @@
+"use strict";
+
+
+function test1() {
+    let a = 10;
+    if (a > 5) {
+        a = 7;
+    }
+    console.log("a = " + a);
+}
+
+test1();
+
+function test2A() {
+    if (1 == 1) {
+    var a = 5;
+    }
+    console.log("a = " + a);
+   }
+   test2A();
+
+   
+   function test2B() {
+    if (1 == 1) {
+    let a = 5;
+    }
+    console.log("a = " + a);
+   }
+   test2B();
+
---
+++ script/strings/parsing.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let Name = "Brenda Kaye";
+let pos = Name.indexOf("Brenda Kaye"); 
+
+let first = Name.substring(0, 6);
+let last = Name.substring(7);
+
+console.log(pos);
+console.log(first);
+console.log(last);
+
