5a6622535652fc2be571adc848c4e54f6dafebd9
jtoxquimaxil <jtoxquimaxil@gmail.com>
2023-11-09 19:39:42 -0500

    completed custom_date_format.js

---
+++ Dates/DateScripts/custom_date_format.js
@@ -0,0 +1,14 @@
+"use strict";
+
+//current date/time using the folowing format
+//day-month-year(weekdayname) ex: 27-julu-2022
+
+let d = new Date();
+
+
+let d2 = new Date();
+
+
+let d3 = new Date();
+
+console.log(`${d2.getDate() + 1}-${d.getMonth() + 1}-${d3.getFullYear() + 1}`);
